cb60f046ec1541830b51f314054f364f38db9518
AaronMcGuirk007 <am12mcgu@siena.edu>
2023-04-10 22:23:48 -0400

    more testing:

--- src/js/script.js
+++ src/js/script.js
@@ -94,13 +94,14 @@ optionsList.forEach(o => {
   });
 });
 
-$(document).ready(function(){
-    $('#myselection').on('change', function(){
-    	var demovalue = $(this).val(); 
-        $("div.myDiv").hide();
-        $("#show"+demovalue).show();
-    });
-});
+function myFunction() {
+    var x = document.getElementById("data");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
