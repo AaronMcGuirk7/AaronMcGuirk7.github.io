17964a2dde97b46438a9baf9070d2c4f8f925832
AaronMcGuirk007 <am12mcgu@siena.edu>
2023-04-10 22:44:07 -0400

    slow but steady

--- src/js/script.js
+++ src/js/script.js
@@ -94,15 +94,6 @@ optionsList.forEach(o => {
   });
 });
 
-function myFunction() {
-    var x = document.getElementById("data");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
